548ec4d285f3dabb1ee2ca1f6a5292d4dc4c7d2d
Rick Kock <rick.92.tlf@gmail.com>
2017-01-25 05:51:14 +0100

    Add a textbox.

--- prototypeForCLI.py
+++ prototypeForCLI.py
@@ -8,14 +8,27 @@
 #
 ###################################################
 
-# Create a layout
-
+# Imports
 from tkinter import *
 
+# Global variables
 root = Tk()
+
+###################################################
+# SETUP
+###################################################
 
 root.minsize(400, 300)
 root.title('OpenThread GUI demo Avans')
 
+###################################################
+# MAIN
+###################################################
+
+# Add textbox
+inputBox = Entry(root, width="20")
+inputBox.grid(row=1, column=1, columnspan=3)
+
 root.grid()
 root.mainloop()
+###################################################
