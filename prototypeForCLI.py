7aa77813378af579e6c9522cc9fb642155a7fb8b
Rick Kock <rick.92.tlf@gmail.com>
2017-01-25 07:11:51 +0100

    Add a listbox.

--- prototypeForCLI.py
+++ prototypeForCLI.py
@@ -10,6 +10,7 @@
 
 # Imports
 from tkinter import *
+import tkinter.ttk as ttk
 
 # Global variables
 root = Tk()
@@ -27,12 +28,18 @@ root.title('OpenThread GUI demo Avans')
 
 # Add textbox
 inputBox = Entry(root, width="20")
-inputBox.grid(row=1, column=1, columnspan=3)
+inputBox.grid(row=0, column=0, columnspan=1)
 
 # Add a text-area representing the output
 threadNetworkScanResults = Text(root, height=5, width=20)
-threadNetworkScanResults.grid(row=8, column=1, columnspan=3)
+threadNetworkScanResults.grid(row=1, column=0, columnspan=1)
 
+# Add a listbox
+listBox = Listbox(root)
+for item in ["listItem1", "listItem2"]:
+    listBox.insert(END, item)
+
+listBox.grid(row=2, column=0, columnspan=1)
 
 root.grid()
 root.mainloop()
