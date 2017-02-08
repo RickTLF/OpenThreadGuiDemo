603a3f64ec039814e310ce65ce8cf71cf8b21dd3
Rick Kock <rick.92.tlf@gmail.com>
2017-02-08 18:26:18 +0100

    Re-define classes, functions and variables.

--- prototypeForCLI.py
+++ prototypeForCLI.py
@@ -17,18 +17,42 @@
 import serial
 from sandScoop import *
 import sandScoop
+import threading
+#--------------------------------------------------
+# Global vars
+#--------------------------------------------------
+dialog_gui = SandScoop('Avans OpenThread Demo')
 #--------------------------------------------------
 
 print(sandScoop.__doc__)
 
-bgColor = '#330033'
+#bgColor = '#330033'
 
-def printSomething():
-    print("Yellow world!")
+#def printSomething():
+    #print("Yellow world!")
 
-# gui setup
-dialog_gui = SandScoop('the title', 200, 200)
-dialog_gui.addTabs()
+
+def setup():
+    """
+    --------------------------------------------------
+    setup
+    --------------------------------------------------
+    Setup the program GUI.
+    --------------------------------------------------
+    """
+
+    # gui setup
+
+
+def update():
+    """
+    --------------------------------------------------
+    createGui
+    --------------------------------------------------
+    Finally, put things together by using grid.
+    --------------------------------------------------
+    """
+    dialog_gui.createGui()
 
 '''dialog_gui.addLabel('some Text', 0, 0)
 dialog_gui.addInputBox('20', 0, 1)
@@ -44,12 +68,13 @@ dialog_gui.addButton('submit', 2, 2, printSomething)
 
 dialog_gui.createTable(3, 16, 4)'''
 
-def doNothing():
-    print("Do nothing.")
+#dialog_gui.addMenu("File", 0)
 
-dialog_gui.addMenu("File", 0)
+import tkinter as tk
+from tkinter import ttk
 
-dialog_gui.createGui()
+
 #print(dialog_gui.createGui.__doc__)
+
 
 
--- sandScoop.py
+++ sandScoop.py
@@ -18,24 +18,123 @@ from tkinter import *
 import tkinter.messagebox
 from tkinter.scrolledtext import ScrolledText
 
-#TODO: Add panes for widgets!
-#TODO: Add labelFrame!
+# TODO: Add panes for widgets!
+# TODO: Add labelFrame!
 
 class SandScoop:
+
+    def displayConfigFrame(self):
+        """
+        --------------------------------------------------
+        displayConfigFrame
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def displayNetworkFrame(self):
+        """
+        --------------------------------------------------
+        displayNetworkFrame
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def displayLightingControl(self):
+        """
+        --------------------------------------------------
+        displayLightingControl
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def updateGui(self):
+        """
+        --------------------------------------------------
+        updateGui
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def saveWidgetId(self):
+        """
+        --------------------------------------------------
+        saveWidgetId
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def displayLastError(self):
+        """
+        --------------------------------------------------
+        displayLastError
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def saveWidgetId(self, id):
+        """
+        --------------------------------------------------
+        saveWidgetId
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def updateWidget(self):
+        """
+        --------------------------------------------------
+        updateWidget
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def isButtonPressed(self):
+        """
+        --------------------------------------------------
+        isButtonPressed
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def validateInput(self, type):
+        """
+        --------------------------------------------------
+        validateInput
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+    def updateProgramData(self):
+        """
+        --------------------------------------------------
+        updateProgramData
+        --------------------------------------------------
+        Setup the program GUI.
+        --------------------------------------------------
+        """
+
+
+    '''classFrames = []
+    classAdditions = []
     cntMenuItems = 0
     btnWidth = "20"
-    windowWidth = 300
-    windowHeight = 300
     root = Tk()
     backgroundColor = 'white'
     menu = Menu(root)
     subMenu = Menu(menu)
 
-    def __init__(self, thisTitle, width, height, color='white'):
-        self.windowWidth = width
-        self.windowHeight = height
+    def __init__(self, thisTitle, color='white'):
+        self.root.geometry('500x300')
         self.root.configure(background=self.backgroundColor)
-        self.root.minsize(self.windowWidth, self.windowHeight)
         self.root.title(thisTitle)
         self.backgroundColor = color
 
@@ -48,16 +147,38 @@ class SandScoop:
     def __doNothing(self):
         print("Ok ok I won't.")
 
-    def addTabs(self):
-        nb = ttk.Notebook(self.root)
-        page1 = ttk.Frame(nb)
-        # second page
-        page2 = ttk.Frame(nb)
-        text = ScrolledText(page2)
-        text.pack(expand=1, fill="both")
-        nb.add(page1, text='One')
-        nb.add(page2, text='Two')
-        nb.pack(expand=1, fill="both")
+    def addTabs(self, frames):
+        notebook = ttk.Notebook(self.root)
+
+        # Add frames
+
+        # For each frame, create frame
+        for frame in range(0, len(frames)-1):
+            self.classFrames[frame] = ttk.Frame(notebook)
+
+        for frame in range(0, len(frames)-1):
+            self.classFrames[frame] = ttk.Frame(notebook)
+            self.classAdditions[frame] = notebook.add(self.classFrames[frame], text=frames[frame])
+
+        frame1 = ttk.Frame(notebook)
+        frame2 = ttk.Frame(notebook)
+        frame3 = ttk.Frame(notebook)
+
+        #Add tabs
+        notebook.add(frame1, text='Frame One')
+        notebook.add(frame2, text='Frame Two')
+        notebook.add(frame3, text='Frame Three')
+        notebook.grid()
+
+        tkLabel = Label(frame1, text=" Hello Python!")
+        tkLabel.grid()
+
+        strVersion = "running Python version "
+        tkLabelVersion = Label(frame2, text=strVersion)
+        tkLabelVersion.grid()
+        strPlatform = "Platform: "
+        tkLabelPlatform = Label(frame2, text=strPlatform)
+        tkLabelPlatform.grid()
 
     def addMenu(self, text, cnt=1):
         self.cntMenuItems = cnt
@@ -116,3 +237,4 @@ class SandScoop:
             if j % 4 == 0:
                 thisRow += 1
             tableCel.grid(row=thisRow, column=j % maxCol, columnspan=1, in_=blankLabel)
+            '''
